74b8e8e102e2c98274db7a8e79faaf5645a8f4f6
Itzelg <35382601+Itzelg@users.noreply.github.com>
2018-02-02 20:48:01 -0600

    Lab 7

---
+++ lab7.js
@@ -0,0 +1,62 @@
+
+document.getElementById("italica").onmouseover= italicas;
+document.getElementById("negrita").onmouseover = negritas;
+document.getElementById("mayus").onmouseover = smallmayus;
+document.getElementById("normal").onmouseover = normal;
+window.setTimeout(tiempo, milliseconds);
+
+
+function italicas(){
+
+	var x=document.getElementsByClassName("cuestionario");
+	for(var i=0;i<x.length;i++){
+		x[i].style.fontStyle = "italic";
+	}
+
+}
+
+function negritas(){
+	var x=document.getElementsByClassName("cuestionario");
+	for(var i=0;i<x.length;i++){
+		x[i].style.fontWeight = "bold";
+	}
+}
+
+function smallmayus(){
+	var x=document.getElementsByClassName("cuestionario");
+	for(var i=0;i<x.length;i++){
+		x[i].style.fontVariant = "small-caps";
+	}
+}
+
+function normal(){
+	var x=document.getElementsByClassName("cuestionario");
+	for(var i=0;i<x.length;i++){
+		x[i].style.fontVariant = "normal";
+		x[i].style.fontWeight = "normal";
+		x[i].style.fontStyle = "normal";
+
+	}
+}
+
+function tiempo(){
+	alert("Lab 6");
+}
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+
+
