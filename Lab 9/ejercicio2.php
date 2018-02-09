8a22d5d88d6bf9449598b920e8f2755bd2eb531f
Itzelg <A01336759@itesm.mx>
2018-02-09 11:18:02 -0600

    Lab 9

---
+++ Lab 9/ejercicio2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Lab 9 - Ejercicio 2</title>
+		<meta charset="utf-8" />
+		
+		<link rel="stylesheet" href="Lab9.css">
+	</head>
+	<body id="body">
+
+		<div id="heading" >
+				<h1>Mediana de arreglo</h1>
+		</div>
+			<section>
+				<div class="cont">
+					<h2>El arreglo 1 es: array(10,50,38,40,20,5)</h2>
+					<h2>El arreglo 2 es: array(100,70,86,40,10)</h2>
+					<?php
+						include "functions.php";
+						ejercicio2();
+					?>
+				</div>
+			</section>
+	</body>
+</html>
---
+++ Lab 9/ejercicio4.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Lab 9 - Ejercicio 4</title>
+		<meta charset="utf-8" />
+		
+		<link rel="stylesheet" href="Lab9.css">
+	</head>
+	<body id="body">
+
+		<div id="heading" >
+				<h1>Tabla de N</h1>
+		</div>
+			<section>
+				<div class="cont">
+					<h2>1. N = 10</h2>
+					<h2>2. N = 20</h2>
+					<?php
+						include "functions.php";
+						ejercicio4();
+					?>
+				</div>
+			</section>
+	</body>
+</html>
